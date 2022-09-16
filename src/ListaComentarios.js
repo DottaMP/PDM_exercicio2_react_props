719c8c3a94f4855d3225e5a4d9f3e0a8f22d4806
May <dotta.may@gmail.com>
2022-09-16 12:29:10 -0300

    apostila04: exercicio react props concluido

--- src/ListaComentarios.js
+++ src/ListaComentarios.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 const ListaComentarios = (props) => {   
     return (
-        <div className="card shadow-sm p-3 mb-5 bg-white rounded">
-            {/* corpo do cartão */}
-            <div className="card-body">{props.children}</div>
+        <div className={estilo.principal}>
+            {props.children}
         </div>   
     )
 }
+const estilo = {
+    principal: 'card shadow-sm p-3 mb-5 bg-white rounded'
+}
+
 export default ListaComentarios;
